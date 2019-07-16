c7a19289cc5d133e6599bbe2a18479d0bf08add2
eventia <eventia@gmail>
2019-07-16 20:28:13 +0900

    Algorithm Test Case [2019-07-16]

--- countMaze.cpp
+++ countMaze.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 
 int n, A[101][101], cnt, Size[101];
+int dx[4]={1,0,-1,0}, dy[4]={0,1,0,-1};
 
 bool safe(int a, int b){
     return (0<=a && a<n) && (0<=b && b<n);
@@ -10,6 +11,17 @@ bool safe(int a, int b){
 bool cmp(int a, int b){
     return a>b;
 }
+
+/*
+void dfs(int a, int b, int c) {
+    A[a][b] = c;
+        for(int i=0; i<4; i++) {
+             if(safe(a+dx[i],b+dy[i]) && A[a+dx[i]][b+dy[i]]==1) {
+                    dfs(a+dx[i], b+dy[i], c);
+             }
+        }
+}
+*/
 
 void dfs(int a, int b, int c){
     A[a][b]=c;
@@ -39,17 +51,19 @@ void solve(){
 
 }
 
-void input(){
-    scanf("%d", &n);
-    for(int i=0; i<n; i++){
-        for (int j=0; j<n; j++) scanf("%d0, &A[i][j]");
-    }
-}
+
 
 void output(){
     printf("%d\n", cnt);
     for(int i=0; i<cnt; i++){
         printf("%d\n", Size[i]);
+    }
+}
+
+void input(){
+    scanf("%d", &n);
+    for(int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) scanf("%d", &A[i][j]);
     }
 }
 
@@ -59,3 +73,5 @@ int main(){
     output();
     return 0;
 }
+
+
